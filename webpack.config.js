12dfdff913aea0af875251a51a36cf462eae7514
Joseph Chamochumbi <sephxd1234@gmail.com>
2019-04-24 01:31:38 +0200

    chore: webpack background and other scripts without optimizing

--- webpack.config.js
+++ webpack.config.js
@@ -12,12 +12,17 @@ function htmlPlugin({ template, chunks, filename }) {
 
 module.exports = {
   entry: {
+    background: path.join(__dirname, "src/background.js"),
     newtab: path.join(__dirname, "src/newtab.js"),
     popup: path.join(__dirname, "src/popup.js")
   },
   output: {
     path: path.join(__dirname, "extension"),
-    filename: "static/js/[name].[chunkhash:8].js",
+    filename: chunkData => {
+      return chunkData.chunk.name === "background"
+        ? "[name].js"
+        : "static/js/[name].[chunkhash:8].js";
+    },
     chunkFilename: "static/js/[name].[chunkhash:8].chunk.js"
   },
   module: {
@@ -63,6 +68,9 @@ module.exports = {
   ],
   resolve: {
     extensions: [".js", ".jsx", ".png"]
+  },
+  optimization: {
+    minimize: false
   },
   devServer: {
     port: 3001
