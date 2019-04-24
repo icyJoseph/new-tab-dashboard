3da89646b099e4c2bd601a99761606e6bd2d937b
Joseph Chamochumbi <sephxd1234@gmail.com>
2019-04-24 02:10:25 +0200

    feat: on mount useEffect to get the current tab

--- src/containers/Add.js
+++ src/containers/Add.js
@@ -1,18 +1,25 @@
-import React, { useContext } from "react";
+import React, { useState, useEffect } from "react";
 import { State } from "context-hook-provider";
-
 import Button from "../components/Button";
-import { ADD_URL } from "../ducks/store";
+import { useAddUrl } from "../customHooks";
+
 export function Add() {
-  const { dispatch } = useContext(State);
-  return (
-    <Button
-      text="add"
-      callback={() =>
-        dispatch({ type: ADD_URL, payload: { id: Date.now(), url: "test" } })
-      }
-    />
-  );
+  const [tabs, setTabs] = useState([]);
+  const adder = useAddUrl();
+
+  const add = () => {
+    const [{ url }] = tabs;
+    return url && adder({ id: url, url });
+  };
+
+  useEffect(() => {
+    chrome.tabs.query({ active: true, currentWindow: true }, tabs => {
+      console.log(tabs);
+      return setTabs(tabs);
+    });
+  }, []);
+
+  return <Button text="add" callback={add} />;
 }
 
 export default Add;
